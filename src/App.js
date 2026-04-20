26c77d1dad19e44b84f9da33a51b3c6c3c797cfe
LIN Forest 00067691 <forest.lin@3ds.com>
2026-04-19 23:30:27 -0400

    Remove Exchanges and News tabs (deprecated APIs)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+import { Homepage, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './App.css';
 
 const App = () => (
@@ -17,23 +17,17 @@ const App = () => (
             <Route exact path="/">
               <Homepage />
             </Route>
-            <Route exact path="/exchanges">
-              <Exchanges />
-            </Route>
             <Route exact path="/cryptocurrencies">
               <Cryptocurrencies />
             </Route>
             <Route exact path="/crypto/:coinId">
               <CryptoDetails />
             </Route>
-            <Route exact path="/news">
-              <News />
-            </Route>
           </Switch>
         </div>
       </Layout>
       <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2026
           <Link to="/">
             Cryptoverse Inc.
           </Link> <br />
@@ -41,8 +35,7 @@ const App = () => (
         </Typography.Title>
         <Space>
           <Link to="/">Home</Link>
-          <Link to="/exchanges">Exchanges</Link>
-          <Link to="/news">News</Link>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
         </Space>
       </div>
     </div>
